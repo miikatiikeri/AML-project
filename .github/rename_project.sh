e8d87111b0c778ece2c92609092425c95303b84e
AnttiYK <AnttiYK@users.noreply.github.com>
2022-11-19 12:20:08 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="AnttiYK"
+original_name="aml_project"
+original_urlname="AML-project"
+original_description="Awesome aml_project created by AnttiYK"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv aml_project $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from aml_project.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "aml_project"
