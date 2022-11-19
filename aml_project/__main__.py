cbe3831e9b1f410caf3dbf532b1dc3d116d6984c
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-11-19 14:44:24 +0200

    init

--- aml_project/__main__.py
+++ aml_project/__main__.py
@@ -1,6 +1,6 @@
 """Entry point for aml_project."""
 
-from .cli import main  # pragma: no cover
+from cli import main  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
     main()
