ffdca6b35bbb2ca017e908747e0c48b4bbda8f4a
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-11-24 15:04:12 +0200

    added dataset

--- aml_project/cli.py
+++ aml_project/cli.py
@@ -1,28 +1,4 @@
-"""CLI interface for aml_project project.
-
-Be creative! do whatever you want!
-
-- Install click or typer and create a CLI app
-- Use builtin argparse
-- Start a web application
-- Import things from your .base module
-"""
 
 
-def main():  # pragma: no cover
-    """
-    The main function executes on commands:
-    `python -m aml_project` and `$ aml_project `.
-
-    This is your program's entry point.
-
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
-    """
+def main():  # pragma: no cover   
     print("This will do something")
