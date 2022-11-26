1d3cfe0a0e80faabb3f0dd6fd5579364a404ced2
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-11-26 18:06:19 +0200

    Workflow

--- aml_project/cli.py
+++ aml_project/cli.py
@@ -1,4 +1,22 @@
+import cv2 as cv
+import tensorflow as tf
+from tensorflow import keras
 
+def main():  # pragma: no cover  
+    print("this will do something") 
+    #read data and labels
 
-def main():  # pragma: no cover   
-    print("This will do something")
+    #visualize data
+
+    #split dataset here or in training? always use set seeds 
+
+    #train model and save it
+
+    #load model
+
+    #predict test data
+
+    #visualize results
+
+    #grad-cam or lime?
+   
