36677791d32c5ffb97a57225e32bb4b57a667064
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-11-27 19:54:26 +0200

    added cnn_model.py

---
+++ aml_project/cnn_model.py
@@ -0,0 +1,39 @@
+#NETWORK
+#layers = [
+
+#imageInputLayer([64, 64, 3]);
+
+#convolution2dLayer([5, 5], 32, 'Padding','same');
+#batchNormalizationLayer;
+#reluLayer;
+#maxPooling2dLayer(2, 'Stride', 2);
+
+#convolution2dLayer([5, 5], 32, 'Padding','same');
+#batchNormalizationLayer;
+#reluLayer;
+#maxPooling2dLayer(2, 'Stride', 2);
+
+#convolution2dLayer([5, 5], 32, 'Padding','same');
+#batchNormalizationLayer;
+#reluLayer;
+#maxPooling2dLayer(2, 'Stride', 2);
+
+#fullyConnectedLayer(128);
+#reluLayer;
+
+#fullyConnectedLayer(2);
+#softmaxLayer;
+#classificationLayer;
+#];
+
+# TRAINING OPTINS
+#options = trainingOptions('sgdm', ...
+#    'MaxEpochs',40, ...
+#    'ValidationData',imdsValidation, ...
+#    'ValidationFrequency',30, ...
+#    'Verbose',false, ...
+#    'Plots','training-progress');
+
+
+# TODO: TRAIN YOUR NETWORK
+#net = trainNetwork(imdsTrain,layers,options);
