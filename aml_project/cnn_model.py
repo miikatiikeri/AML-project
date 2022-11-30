05b36292aa43361409b83280d84e5a77e9e05c1c
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-11-30 16:26:30 +0200

    removed excess comments

--- aml_project/cnn_model.py
+++ aml_project/cnn_model.py
@@ -1,43 +1,3 @@
-#NETWORK
-#layers = [
-
-#imageInputLayer([64, 64, 3]);
-
-#convolution2dLayer([5, 5], 32, 'Padding','same');
-#batchNormalizationLayer;
-#reluLayer;
-#maxPooling2dLayer(2, 'Stride', 2);
-
-#convolution2dLayer([5, 5], 32, 'Padding','same');
-#batchNormalizationLayer;
-#reluLayer;
-#maxPooling2dLayer(2, 'Stride', 2);
-
-#convolution2dLayer([5, 5], 32, 'Padding','same');
-#batchNormalizationLayer;
-#reluLayer;
-#maxPooling2dLayer(2, 'Stride', 2);
-
-#fullyConnectedLayer(128);
-#reluLayer;
-
-#fullyConnectedLayer(2);
-#softmaxLayer;
-#classificationLayer;
-#];
-
-# TRAINING OPTINS
-#options = trainingOptions('sgdm', ...
-#    'MaxEpochs',40, ...
-#    'ValidationData',imdsValidation, ...
-#    'ValidationFrequency',30, ...
-#    'Verbose',false, ...
-#    'Plots','training-progress');
-
-
-# TTRAIN YOUR NETWORK
-#net = trainNetwork(imdsTrain,layers,options);
-
 import tensorflow as tf
 from tensorflow import keras
 from read_data import read_data, read_labels
@@ -50,27 +10,9 @@ def split_data(data):
         )
     return train_ds, test_ds
 
-# def extract(dataset):
-#     images = list()
-#     labels = list()
-#     for i, l in dataset:
-#         images.append(i)
-#         labels.append(l)
-#     return images, labels
 
 
 def model(train_ds, test_ds, train_smile_ds, test_smile_ds):
-    # model structure
-    # model = models.Sequential()
-    # model.add(layers.Conv2D(32, 3, activation='relu'))
-    # model.add(layers.MaxPooling2D())
-    # model.add(layers.Conv2D(32, 3, activation='relu'))
-    # model.add(layers.MaxPooling2D())
-    # model.add(layers.Conv2D(32, 3, activation='relu'))
-    # model.add(layers.MaxPooling2D())
-    # model.add(layers.Flatten())
-    # model.add(layers.Dense(128, activation='relu'))
-    # model.add(layers.Dense(2))
     
     num_classes = 2
     model = tf.keras.Sequential([
@@ -91,13 +33,6 @@ def model(train_ds, test_ds, train_smile_ds, test_smile_ds):
     model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-
-    # model summary
-    #model.summary()
-
-    #extract images and labels from dataset
-    #train_images, train_labels = extract(train_ds)
-    #test_images, test_labels = extract(test_ds)
 
     #train model
     model.fit(train_smile_ds, 
