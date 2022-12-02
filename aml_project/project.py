ff367cbc3dcf723583ae66fcead6b94fd1d6e796
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-12-02 10:42:04 +0200

    Read data fixed

--- aml_project/project.py
+++ aml_project/project.py
@@ -8,8 +8,8 @@ import platform
 # project imports
 from read_data import read_data, read_labels
 import plotting;
-import labels
 import cnn_model
+
 # global variables
 # determines the size that images get scaled in read_data
 # labels also get scaled accordingly
@@ -18,30 +18,30 @@ scaled_size = 32
 normalize = True
 # number of epochs for training, set higher when actually training model
 n_epochs = 3
+# quick fix for dataread
+user = False
 
 
 def main():  # pragma: no cover
 
-    # platform detection to fix read issues between os
-    if platform.system() == "Linux" or "Darwin":
+    #platform detection to fix read issues between os
+    if (platform.system() == "Linux" or "Darwin") and user != True:
       print("using unix read")
-      data_smile = read_data("../dataset/GENKI-R2009a/Subsets/GENKI-4K",
-                            "../dataset/GENKI-R2009a/Subsets/GENKI-4K/GENKI-4K_Labels.txt", 's', scaled_size, normalize)
-      data_face = read_data("../dataset/GENKI-R2009a/Subsets/GENKI-SZSL",
-                            "../dataset/GENKI-R2009a/Subsets/GENKI-SZSL/GENKI-SZSL_labels.txt", 'f', scaled_size, normalize)
+      images, face_labels, smile_labels = read_data("../dataset/GENKI-R2009a/Subsets/GENKI-SZSL",
+                        "../dataset/GENKI-R2009a/Subsets/GENKI-SZSL/GENKI-SZSL_labels.txt",
+                        "../dataset/GENKI-R2009a/Subsets/GENKI-SZSL/smile_labels.txt",
+                        scaled_size, normalize, user)
     else:
       print("using windows read")
       # read data and labels s for smile, f for face
       # data_smile contains images and labels, labels are in array where N:th label corresponds to N:th image
       # 1 = smiling, 0 = not smiling
-      data_smile = read_data("dataset/GENKI-R2009a/Subsets/GENKI-4K",
-                             "dataset/GENKI-R2009a/Subsets/GENKI-4K/GENKI-4K_Labels.txt", 's', scaled_size, normalize)
       # data_face contains images and labels, labels are in array where N:th label corresponds to N:th image
       # each indice in labels contains subarray where l[i][0] = x cordinate of center of face, l[i][1] = y cordinate of center of face, l[i][2] = box size
-      data_face = read_data("dataset/GENKI-R2009a/Subsets/GENKI-SZSL",
-                            "dataset/GENKI-R2009a/Subsets/GENKI-SZSL/GENKI-SZSL_labels.txt", 'f', scaled_size, normalize)
-
-  
+      images, face_labels, smile_labels = read_data("dataset/GENKI-R2009a/Subsets/GENKI-SZSL",
+                        "dataset/GENKI-R2009a/Subsets/GENKI-SZSL/GENKI-SZSL_labels.txt",
+                        "dataset/GENKI-R2009a/Subsets/GENKI-SZSL/smile_labels.txt", 
+                        scaled_size, normalize, user)
     
     # visualize data
     #plotting.plot_pixels(data_smile)
--- aml_project/read_data.py
+++ aml_project/read_data.py
@@ -4,7 +4,7 @@ import os
 import cv2 as cv
 import platform
 
-def read_labels(y_path, type, sz):
+def read_labels(y_path, type, scaled_size, user):
     y = list()
     #read labels
     data = open(y_path, 'r').read().splitlines()
@@ -16,51 +16,55 @@ def read_labels(y_path, type, sz):
     elif(type == 'f'):
         for d in data:
             y.append([int(d.split(" ")[0]), int(d.split(" ")[1]), int(d.split(" ")[2])])
-        fix_labels(y, sz)
+        fix_labels(y, scaled_size, user)
     return y
 
 #fixes the label scaling
-def fix_labels(labels, new_size):
-    if platform.system() == "Linux" or "Darwin":
+def fix_labels(labels, scaled_size, user):
+    if (platform.system() == "Linux" or "Darwin") and user != True:
         dir = "../dataset/GENKI-R2009a/Subsets/GENKI-SZSL/files/"
     else:
         dir = "dataset/GENKI-R2009a/Subsets/GENKI-SZSL/files/"
-
     i = 0
     for image in sorted(os.listdir(dir)):
         im = cv.imread(os.path.join(dir, image))
         dimensions = im.shape
         x_ratio = labels[i][0] / dimensions[0]
         y_ratio = labels[i][1] / dimensions[1]
-        box_ratio = ((new_size / dimensions[0]) + (new_size / dimensions[1])) / 2
-        labels[i][0] = int(new_size * x_ratio)
-        labels[i][1] = int(new_size * y_ratio)
+        box_ratio = ((scaled_size / dimensions[0]) + (scaled_size / dimensions[1])) / 2
+        labels[i][0] = int(scaled_size * x_ratio)
+        labels[i][1] = int(scaled_size * y_ratio)
         labels[i][2] = int(labels[i][2] * box_ratio)
         i = i + 1
 
 
 
-def read_data(X_path, y_path, type, new_size, normalize):
-    y = read_labels(y_path, type, new_size)
+def read_data(image_path, face_path, smile_path, scaled_size, normalize, user):
+    face_labels = read_labels(face_path, "f", scaled_size, user)
+    smile_labels = read_labels(smile_path, "s", scaled_size, user)
     X = list()
     data = keras.utils.image_dataset_from_directory(
-        X_path,
-        labels = y,
+        image_path,
+        labels = None,
         label_mode = 'int',
         class_names = None,
         color_mode = 'rgb',
         batch_size = 32,
-        image_size = [new_size, new_size],
+        image_size = [scaled_size, scaled_size],
         shuffle = True,
         seed = 1,
         validation_split = None,
         interpolation = 'bilinear',
         follow_links = False,
         crop_to_aspect_ratio = False
-    )  
+    )
+    img = list()
+    for images in data:
+        for i in images:
+            img.append(i)
     #normalize images
-    if(normalize):
-        for images, labels in data:
-            for im in images:
-                im = im /255
-    return data 
+    # if(normalize):
+    #     for images, labels in data:
+    #         for im in images:
+    #             im = im /255
+    return img, face_labels, smile_labels
